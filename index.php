6fa7c9004c237c8d4c8da19db856b861cd15443b
Aefvvges <torri.barbara4@gmail.com>
2024-04-19 21:27:10 -0300

    com2

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ $numero1 = $_GET['numero1'];
 $numero2 = $_GET['numero2'];
 $numero3 = $numero1 + $numero2;
 
-echo 'La suma del número ' . $numero1 . ' y del número ' . $numero2 . ' es igual a ' . $numero3
+echo 'La suma del número ' . $numero1 . ' y del número ' . $numero2 . ' es igual a ' . $numero3;
